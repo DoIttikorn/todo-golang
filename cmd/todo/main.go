c00b4e3e7928d0a0691cf064925829b8d1bc6f3e
Doittikorn <aseaways1234@gmail.com>
2022-06-27 03:19:12 +0700

    complete CRUD todo

--- cmd/todo/main.go
+++ cmd/todo/main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"bufio"
+	"errors"
 	"flag"
 	"fmt"
+	"io"
 	"os"
+	"strings"
 
 	"github.com/doittikorn/todo-golang"
 )
@@ -15,6 +19,8 @@ const (
 func main() {
 	add := flag.Bool("add", false, "add a new task")
 	complete := flag.Int("complete", 0, "mark a task as completed")
+	del := flag.Int("del", 0, "delete a task")
+	list := flag.Bool("list", false, "list all tasks")
 
 	flag.Parse()
 
@@ -27,8 +33,14 @@ func main() {
 
 	switch {
 	case *add:
-		todos.Add("Sample todo")
-		err := todos.Store(todoFile)
+		task, err := getInput(os.Stdin, flag.Args()...)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+
+		todos.Add(task)
+		err = todos.Store(todoFile)
 		if err != nil {
 			fmt.Fprintln(os.Stderr, err.Error())
 			os.Exit(1)
@@ -44,9 +56,41 @@ func main() {
 			fmt.Fprintln(os.Stderr, err.Error())
 			os.Exit(1)
 		}
+	case *del > 0:
+		err := todos.Delete(*del)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+		err = todos.Store(todoFile)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+	case *list:
+		todos.Print()
 	default:
 		fmt.Fprintln(os.Stdout, "invalid command")
 		os.Exit(1)
 	}
 
+}
+
+func getInput(r io.Reader, args ...string) (string, error) {
+
+	if len(args) > 0 {
+		return strings.Join(args, " "), nil
+	}
+	scanner := bufio.NewScanner(r)
+	scanner.Scan()
+
+	if err := scanner.Err(); err != nil {
+		return "", err
+	}
+	text := scanner.Text()
+	if len(text) == 0 {
+		return "", errors.New("empty todo is not allowed")
+	}
+
+	return text, nil
 }
--- todo.go
+++ todo.go
@@ -3,9 +3,12 @@ package todo
 import (
 	"encoding/json"
 	"errors"
+	"fmt"
 	"io/ioutil"
 	"os"
 	"time"
+
+	"github.com/alexeyco/simpletable"
 )
 
 type item struct {
@@ -76,4 +79,57 @@ func (t *Todos) Store(filename string) error {
 	}
 
 	return ioutil.WriteFile(filename, data, 0644)
+}
+
+func (t *Todos) Print() {
+	table := simpletable.New()
+
+	table.Header = &simpletable.Header{
+		Cells: []*simpletable.Cell{
+			{Align: simpletable.AlignCenter, Text: "#"},
+			{Align: simpletable.AlignCenter, Text: "Task"},
+			{Align: simpletable.AlignCenter, Text: "Done?"},
+			{Align: simpletable.AlignRight, Text: "Created At"},
+			{Align: simpletable.AlignRight, Text: "Completed At"},
+		},
+	}
+	var cells [][]*simpletable.Cell
+
+	for i, item := range *t {
+
+		task := blue(item.Task)
+		done := blue("NO")
+		if item.Done {
+			task = green(fmt.Sprintf("\u2705 %s", item.Task))
+			done = green("YES")
+		}
+
+		cells = append(cells, *&[]*simpletable.Cell{
+			{Align: simpletable.AlignCenter, Text: fmt.Sprintf("%d", i+1)},
+			{Align: simpletable.AlignLeft, Text: task},
+			{Align: simpletable.AlignCenter, Text: done},
+			{Align: simpletable.AlignRight, Text: item.CreatedAt.Format(time.RFC822)},
+			{Align: simpletable.AlignRight, Text: item.CompletedAt.Format(time.RFC822)},
+		})
+	}
+
+	table.Body = &simpletable.Body{Cells: cells}
+
+	table.Footer = &simpletable.Footer{Cells: []*simpletable.Cell{
+		{Align: simpletable.AlignCenter, Span: 5, Text: red(fmt.Sprintf("%d pending tasks", t.CountPending()))},
+	}}
+
+	table.SetStyle(simpletable.StyleUnicode)
+
+	table.Println()
+}
+
+func (t *Todos) CountPending() int {
+	var count int
+	for _, item := range *t {
+		if !item.Done {
+			count++
+		}
+	}
+	return count
 }
